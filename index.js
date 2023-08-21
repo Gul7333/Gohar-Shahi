81a90381da63947e2c4b31b4b0286fbca92509fa
Gul7333 <g.task.pk@gmail.com>
2023-08-21 11:55:32 +0500

    firt commit

---
+++ index.js
@@ -0,0 +1,132 @@
+const body = document.querySelector("body");
+const popUp = document.getElementById("popup");
+const popUpImg = document.querySelector("#popup .popupimg");
+const popImg = document.querySelectorAll(".slideimg");
+function openPopUp(imageSrc) {
+  popUp.style.display = "block";
+  popUpImg.src = imageSrc;
+  body.style.overflow = "hidden";
+  console.log("openPopUp clicked");
+}
+function closePopUp() {
+  popUp.style.display = "none";
+  body.style.overflow = "auto";
+}
+popImg.forEach(function (item) {
+  item.addEventListener("click", () => openPopUp(item.src));
+});
+
+// __________________________________
+// navigation Popup effect
+
+const navPop = document.querySelector(".nav-pop");
+const Logo = document.querySelector(".Logo");
+function navtoggle() {
+  navPop.classList.toggle("activenav");
+  Logo.classList.toggle("Logo-cross");
+  body.classList.toggle("body-hidden");
+}
+// ________________________________
+// slide effect for quotes
+const quote = document.querySelectorAll(".animated-Quote");
+
+var currentIndex = 0;
+function showSlide(index) {
+  quote.forEach(function (card) {
+    card.classList.remove("active2");
+  });
+  quote[index].classList.add("active2");
+}
+
+function nextCard() {
+  currentIndex++;
+  if (currentIndex >= quote.length) {
+    currentIndex = 0;
+  }
+  showSlide(currentIndex);
+}
+showSlide(0);
+
+setInterval(nextCard, 6000);
+// ________________________________
+//rotate slide effect on profile photo
+
+const photoArray = [
+  "img0",
+  "img1",
+  "img2",
+  "img4",
+  "img5",
+  "img6",
+  "img7",
+  "img8",
+];
+const photo = document.querySelector(".photo");
+fetch("Profile-photo.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("fetch request start");
+    console.log(data);
+    data.forEach((item) => {
+      const img = document.createElement("img");
+      img.classList.add("animatePhoto");
+      img.src = "images/" + item + ".jpg";
+
+      photo.appendChild(img);
+    });
+    const profilePhoto = document.querySelectorAll(".animatePhoto");
+    console.log(profilePhoto + "ggg");
+    var currentPhoto = 0;
+    function showProfilePhoto(index) {
+      profilePhoto.forEach((item) => {
+        item.classList.remove("active");
+      });
+      profilePhoto[index].classList.add("active");
+    }
+    function nextProfilePhoto() {
+      currentPhoto++;
+      if (currentPhoto >= profilePhoto.length) {
+        currentPhoto = 0;
+      }
+      showProfilePhoto(currentPhoto);
+    }
+    showProfilePhoto(0);
+    setInterval(nextProfilePhoto, 5000);
+    // fetch request end
+  });
+// _________________scrolltotop______
+
+// "scroll to top" which appear after scrollY higher then 800
+
+const scrollTotop = document.querySelector(".scrolltotop");
+window.addEventListener("scroll", () => {
+  if (scrollY > 800) {
+    scrollTotop.style.display = "block";
+    console.log(scrollY);
+  } else {
+    scrollTotop.style.display = "none";
+  }
+});
+function scrollToTop() {
+  window.scroll(0, 0);
+}
+// js for showing team Members in footer
+// first i fetch team.json file which contains name and photo url of team
+// member
+const team = document.querySelector(".team");
+//accessing template div in footer
+const teamMemberBox = document.getElementById("team-member-box");
+
+fetch("Team.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((array) => {
+      console.log(array[1]);
+      const teamImg = teamMemberBox.content.cloneNode(true);
+      var img = teamImg.querySelector(".image-box img");
+      img.src = array[1];
+      var title = teamImg.querySelector(".image-title");
+      title.innerText = array[0];
+      team.appendChild(teamImg);
+    });
+  });
