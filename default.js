5d677282c1f17e5bb80d8b469882c585c5996b42
Gul7333 <g.task.pk@gmail.com>
2024-12-08 15:33:34 +0500

    jj

--- default.js
+++ default.js
@@ -75,26 +75,3 @@ function navtoggle() {
 //   navItem[activeLink].classList.add("activeNavItem")
 // }
 // __________________________________
-// js for showing team Members in footer
-// first i fetch team.json file which contains name and photo url of team
-// member
-const team = document.querySelector(".team");
-//accessing template div in footer
-const teamMemberBox = document.getElementById("team-member-box");
-
-fetch("Team.json")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((array) => {
-      const teamImg = teamMemberBox.content.cloneNode(true);
-      var img = teamImg.querySelector(".image-box img");
-      img.src = array[1];
-      var title = teamImg.querySelector(".image-title");
-      title.innerHTML = `<a href="https://www.facebook.com/riazul.jannah" >${array[0]}</a>`;
-      team.appendChild(teamImg);
-    });
-  });
-  document.addEventListener('DOMContentLoaded', function () {
-    initReadMore('.readmore');
-  });
-  
